364857f5d6fb3ba8728de5abd6e67abff490798a
Ali <ali.arioglu06@gmail.com>
2023-07-23 13:10:42 +0300

    first commit

---
+++ src/components/Notification.tsx
@@ -0,0 +1,9 @@
+const Notification = () => {
+  return (
+    <div className="h-12 bg-red-500 text-white px-4 text-center text-sm md:text-base flex items-center justify-center">
+      Free delivery for all orders over $50. Order your food now!
+    </div>
+  );
+};
+
+export default Notification;
